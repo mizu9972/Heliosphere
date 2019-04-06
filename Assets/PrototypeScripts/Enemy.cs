49938fee2fe57bc950da75849df80c8b4a285688
mizu9972 <oisiimizu9972@icloud.com>
2019-04-06 20:55:49 +0900

    Assets/Prototypescriptsにプロトタイプ版でしようしたスクリプトのコピーを追加

---
+++ Assets/PrototypeScripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform TargetTrans = null;
+    private NavMeshAgent NMAgent = null;
+
+    private void Awake()
+    {
+        NMAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TargetTrans != null)
+        {
+            NMAgent.destination = TargetTrans.position;
+        }
+    }
+}
