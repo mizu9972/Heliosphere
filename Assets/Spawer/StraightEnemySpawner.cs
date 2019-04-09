1ab3e594d53726583d25522097a7809dd68475e3
mizu9972 <oisiimizu9972@icloud.com>
2019-04-09 15:13:05 +0900

    スポナーのエネミー生成数に制限を付けました。

--- Assets/Spawer/StraightEnemySpawner.cs
+++ Assets/Spawer/StraightEnemySpawner.cs
@@ -10,7 +10,9 @@ public class StraightEnemySpawner : MonoBehaviour
 
     public GameObject SpawnEnemy;//出現させるエネミー
     public float IntervalTime;//出現させる間隔
+    public int MaxEnemyNum;//軟体出現させるか
     private float CountTime;//時間管理用変数
+    private float CountEnemyNum = 0;//出現させたエネミーカウント用
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +22,33 @@ public class StraightEnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //指定時間ごとにエネミーを生成
         CountTime += Time.deltaTime;
         if(CountTime >= IntervalTime)
         {
-            Instantiate(SpawnEnemy);
-
+            EnemySpawn();//エネミー出現
             CountTime = 0;
+        }
+
+        SpawnerManager();
+    }
+
+    void EnemySpawn()
+    {
+        //エネミー出現処理
+        Instantiate(SpawnEnemy);
+        CountEnemyNum += 1;
+
+    }
+
+    void SpawnerManager()
+    {
+        //スポナー管理
+
+        //出現させたエネミーの数が指定数に達したら処理をストップ 
+        if(CountEnemyNum >= MaxEnemyNum)
+        {
+            enabled = false;
         }
     }
 }
