3d706a3f3db20e0d62af1bb5827288bba1f4f59c
mizu9972 <oisiimizu9972@icloud.com>
2019-04-13 00:24:19 +0900

    PrototypeScriptsフォルダをPrototypeFolderに変更Enemy・SpawnerをPrototypeFolder内に移動

---
+++ Assets/PrototypeFolder/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+
+    private Vector3 MoveVector = Vector3.zero;
+    private CharacterController MyController;
+    private Rigidbody MyRigidB;
+    private Transform MyTrans;
+    private SphereCollider MyCollider;
+    public float Speed = 2.0f;
+    public float Gravity = 9.8f;
+    public float SizeUp = 2.0f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MyController = GetComponent<CharacterController>();
+        MyRigidB = this.GetComponent<Rigidbody>();
+        MyTrans = this.transform;
+        MyCollider = this.GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var MoveHorizontal = Input.GetAxis("Horizontal");
+        var MoveVertical = Input.GetAxis("Vertical");
+
+        MoveVector = new Vector3(MoveHorizontal,  0, MoveVertical);
+        MoveVector = transform.TransformDirection(MoveVector);
+        MoveVector *= Speed;
+        MoveVector.y -= Gravity * Time.deltaTime;
+
+        MyController.Move(MoveVector * Time.deltaTime);
+ 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            //サイズ変更
+            Transform GetTrans = this.transform;
+            Vector3 SetScale = GetTrans.lossyScale;
+            Vector3 SizeUpScale = new Vector3(SetScale.x * SizeUp, SetScale.y * SizeUp, SetScale.z * SizeUp);
+            MyTrans.localScale = SizeUpScale;
+
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/PrototypeFolder/Spawer/StraightEnemySpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//-------------------------------------------------------------
+//直進するエネミーを一定時間ごとに出現させるスポナースクリプト
+//-------------------------------------------------------------
+
+public class StraightEnemySpawner : MonoBehaviour
+{
+    public float StartTime;//出現開始時間
+    public GameObject SpawnEnemy;//出現させるエネミー
+    public float IntervalTime;//出現させる間隔
+    public int MaxEnemyNum;//軟体出現させるか
+
+    private float StartCountTime;//起動までの時間管理変数
+    private float CountTime;//時間管理用変数
+
+    [SerializeField]
+    private float CountEnemyNum = 0;//出現させたエネミーカウント用
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCountTime = 0;
+        CountTime = IntervalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCountTime += Time.deltaTime;
+
+        if (StartCountTime >= StartTime)
+        {
+            EnemySpawn();//エネミー生成
+        }
+
+        SpawnerManager();//スポナー管理
+
+    }
+
+    void EnemySpawn()
+    {
+
+        //指定時間ごとにエネミーを生成
+        CountTime += Time.deltaTime;
+        if (CountTime >= IntervalTime)
+        {
+            GameObject enemy;
+            //エネミー出現処理
+            enemy = Instantiate(SpawnEnemy, this.transform/*.position,this.transform.rotation*/);
+            CountEnemyNum += 1;
+            CountTime = 0;
+            //enemy.GetComponent<EnemyScript>().GetSpawner(gameObject);
+        }
+
+
+    }
+
+    void SpawnerManager()
+    {
+        //スポナー管理
+
+        //出現させたエネミーの数が指定数に達したら処理をストップ 
+        if(CountEnemyNum >= MaxEnemyNum)
+        {
+            enabled = false;
+        }
+    }
+}
