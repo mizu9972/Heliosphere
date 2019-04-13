179a2ac57dac0261e65f55ac5e221a9b78ff1ea2
syu_0417 <s_kiba84@yahoo.co.jp>
2019-04-14 04:33:18 +0900

    競合でやらかした臭がしたので最後に更新された阿形データに木林データをドッキング

---
+++ Assets/Game/Comet/Comet/Gizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    public float GizmoSize = 0.3f;//ギズモのサイズ
+    public Color GizmoColor = Color.yellow;//ギズモの色
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = GizmoColor;//色をセット
+        Gizmos.DrawWireSphere(transform.position, GizmoSize);//描画
+    }
+}
