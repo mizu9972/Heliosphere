581d88dba92f3613ae30f3021f7fb826bf829153
mizu9972 <oisiimizu9972@icloud.com>
2019-04-13 14:46:55 +0900

    既存のEnemyScriptをprotoEnemyScriptに名称変更

--- Assets/PrototypeFolder/Enemy/StraightEnemy/protoEnemyScript.cs
+++ Assets/PrototypeFolder/Enemy/StraightEnemy/protoEnemyScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class protoEnemyScript : MonoBehaviour
 {
     
     private GameObject Spawner;//発生するスポナー
