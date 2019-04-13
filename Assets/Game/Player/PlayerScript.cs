94864f196eb9b9c883abc380e1b6413551b2cb34
mizu9972 <oisiimizu9972@icloud.com>
2019-04-13 22:39:20 +0900

    Start表示の後に各オブジェクトが行動を行うように実装。PlayerのCore,RevolutionCore,CometにisActive変数を置き、それぞれのisActiveを操作するインターフェース実装。

--- Assets/Game/Player/PlayerScript.cs
+++ Assets/Game/Player/PlayerScript.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;//シーン遷移に必要
 //[RequireComponent(typeof(Rigidbody))]//リジッドボディのコンポーネントをオブジェクトに追加
-public class PlayerScript : MonoBehaviour
+
+public class PlayerScript : MonoBehaviour, ChangeActiveInterface
 {
 
     public float speed = 3.0f; //速さ
     
     public float Gravity = 9.8f;
-
-    /*private Rigidbody rb;*/ // Rididbody
 
     protected Vector3 MoveVector = Vector3.zero;
     
@@ -18,19 +17,23 @@ public class PlayerScript : MonoBehaviour
 
     public bool isActive = false;
 
+    private float MoveHorizontal;
+    private float MoveVertical;
     // Start is called before the first frame update
     void Start()
     {
         // コンポーネントを取得
         controller = GetComponent<CharacterController>();
-        //rb = GetComponent<Rigidbody>();
         
     }
     // Update is called once per frame
     void Update()
     {
-        var MoveHorizontal = Input.GetAxis("Horizontal");
-        var MoveVertical = Input.GetAxis("Vertical");
+        if (isActive)
+        {
+            //isActiveがtrueなら操作受付
+            Controll();
+        }
 
         MoveVector = new Vector3(MoveHorizontal, 0, MoveVertical);
         MoveVector = transform.TransformDirection(MoveVector);
@@ -38,5 +41,17 @@ public class PlayerScript : MonoBehaviour
         MoveVector.y -= Gravity * Time.deltaTime;
 
         controller.Move(MoveVector * Time.deltaTime);
+    }
+
+    private void Controll()
+    {
+        //キー操作
+        MoveHorizontal = Input.GetAxis("Horizontal");
+        MoveVertical = Input.GetAxis("Vertical");
+    }
+
+    public void ChangeActive()
+    {
+        isActive = !isActive;
     }
 }
