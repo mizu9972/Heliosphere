39ee4d5eecd657676b637972c9cb0fa3b1532aea
mizu9972 <oisiimizu9972@icloud.com>
2019-04-15 00:32:02 +0900

    タイトル・リザルトシーン追加シーン遷移実装

---
+++ Assets/GameOver/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOverManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            //エンターキーでシーン遷移(仮)
+            SceneChange();
+        }
+    }
+    private void SceneChange()
+    {
+        //シーン遷移
+        SceneManager.LoadScene("TitleScene");
+    }
+}
