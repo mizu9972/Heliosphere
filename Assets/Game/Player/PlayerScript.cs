daf4ee93de71db85bc328f1b5b9e7f079dda9ac4
mizu9972 <oisiimizu9972@icloud.com>
2019-04-16 11:16:15 +0900

    移動が滑るようになっていたのを修正

--- Assets/Game/Player/PlayerScript.cs
+++ Assets/Game/Player/PlayerScript.cs
@@ -9,7 +9,7 @@ public class PlayerScript : MonoBehaviour, ChangeActiveInterface, IMoveOperate
 
     public float speed = 3.0f; //速さ
     
-    public float Gravity = 9.8f;
+    public float Gravity = 9.8f;//重力
 
    
 
@@ -32,12 +32,6 @@ public class PlayerScript : MonoBehaviour, ChangeActiveInterface, IMoveOperate
     void Update()
     {
 
-        MoveVector = new Vector3(MoveHorizontal, 0, MoveVertical);
-        MoveVector = transform.TransformDirection(MoveVector);
-        MoveVector *= speed;
-        MoveVector.y -= Gravity * Time.deltaTime;
-
-        controller.Move(MoveVector * Time.deltaTime);
 
     }
 
@@ -48,12 +42,26 @@ public class PlayerScript : MonoBehaviour, ChangeActiveInterface, IMoveOperate
             //キー操作
             MoveHorizontal = Input.GetAxis("Horizontal");
             MoveVertical = Input.GetAxis("Vertical");
+        }else
+        {
+            //キー入力を受け付けていないときは移動成分を0に
+            MoveHorizontal = 0;
+            MoveVertical = 0;
         }
+
+        //移動反映
+        MoveVector = new Vector3(MoveHorizontal, 0, MoveVertical);
+        MoveVector = transform.TransformDirection(MoveVector);
+        MoveVector *= speed;//速度補正
+
+        MoveVector.y -= Gravity * Time.deltaTime;//重力反映
+
+        controller.Move(MoveVector * Time.deltaTime);//移動
     }
 
     public void ChangeActive()
     {
-        isActive = !isActive;
+        isActive = !isActive;//切り替え
     }
     
 }
