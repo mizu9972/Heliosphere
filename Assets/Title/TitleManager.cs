d638acb8e30b62d6a7f4a1419bf12f78b2f4acc1
mizu9972 <oisiimizu9972@icloud.com>
2019-04-17 11:39:35 +0900

    ジョイスティックのボタン設定 ボタン入力インターフェース追加

--- Assets/Title/TitleManager.cs
+++ Assets/Title/TitleManager.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class TitleManager : MonoBehaviour
+using UniRx;
+using UniRx.Triggers;
+public class TitleManager : MonoBehaviour, IButtonPush
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.UpdateAsObservable().Where(_ => (Input.anyKey)).Subscribe(_ => this.GetComponent<IButtonPush>().ButtonPush());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Return))
-        {
-            //エンターキーでシーン遷移(仮)
-            SceneChange();
-        }
+
     }
 
     private void SceneChange()
@@ -25,4 +23,13 @@ public class TitleManager : MonoBehaviour
         //シーン遷移
         SceneManager.LoadScene("GameSceneno1");
     }
+
+    public void ButtonPush()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            SceneChange();
+        }
+    }
+
 }
