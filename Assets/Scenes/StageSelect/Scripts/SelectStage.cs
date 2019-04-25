e228d862fc743de53c248e357197edfb9b6b7595
syu_0417 <s_kiba84@yahoo.co.jp>
2019-04-25 13:52:55 +0900

    カーソルを動かして選択されたオブジェクトのテクスチャの座標を切り替える処理を実装。 ステージ選択画面として編集可能にするためにオブジェクトはプレハブ化。

---
+++ Assets/Scenes/StageSelect/Scripts/SelectStage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelectStage
+{
+    //カーソルと重なった時に実行
+    void OnSelect();
+    //カーソルが離れた時に実行
+    void RemoveSelect();
+    //選択された状態でボタンを押したらscene遷移s
+}
