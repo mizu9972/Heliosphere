39e97ad6b9fc20221771760e45a9b1c36731afe9
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-11 00:04:25 +0900

    stage1を仮でTemplateに変更。ステージセレクトで背景とcursorが衝突してたのでPlaneのコライダーをオフに。音楽の再生処理をupdate()内で一度のみ実行するように変更。それに伴ってシーンが切り替わるたびに処理用のフラグをfalseにする処理を実装。 クリア、ゲームオーバーのシーンの場合は仮でSE置いてます。SEの素材がないので爆発SEで代用しています。

--- Assets/Scenes/Manager/Scripts/AudioManager.cs
+++ Assets/Scenes/Manager/Scripts/AudioManager.cs
@@ -9,52 +9,72 @@ public class AudioManager : MonoBehaviour
     public AudioClip TitleBgm;//タイトルBGM
     public AudioClip GameMainBgm;//ゲームメインBGM
     public AudioClip MenuBgm;//ステージセレクト(仮)
+    public AudioClip ClearSound;//ゲームCLEAR
+    public AudioClip OverSound;//ゲームオーバー
 
     private AudioSource audioSource;
     private string sceneName;//アクティブシーン名
     private bool PlayFlg = false;
     void Start()
     {
+        SceneManager.activeSceneChanged += ActiveSceneChanged;
         audioSource = gameObject.GetComponent<AudioSource>();
         sceneName = SceneManager.GetActiveScene().name;//アクティブシーン名の取得
         
     }
     private void Update()
     {
-        sceneName = SceneManager.GetActiveScene().name;//アクティブシーン名の取得
+        ChangeClip();
+        if(!PlayFlg)//一度のみ再生(ループ設定がtrueになっていれば自動で再生される)
+        {
+            audioSource.Play();
+            PlayFlg = true;
+        }
+        
+    }
+
+    void ChangeClip()
+    {
+        //sceneName = SceneManager.GetActiveScene().name;//アクティブシーン名の取得
         //シーンごとにBGM切り替え
         if (sceneName == "TitleScene")//タイトルシーン
         {
             audioSource.clip = TitleBgm;
+            audioSource.loop = true;
         }
+
         else if (sceneName == "StageSelectScene")//ステージセレクトシーン
         {
-            //ステージ選択のBGM
+            //ステージ選択のBGM(ループする)
             audioSource.clip = MenuBgm;
+            audioSource.loop = true;
         }
+
+        else if (sceneName == "StageClearScene")//ゲームCLEAR
+        {
+            //ゲームCLEARのBGM(ループしない)
+            audioSource.clip = ClearSound;
+            audioSource.loop = false;
+        }
+
+        else if (sceneName == "GameOverScene")//ゲームオーバー
+        {
+            //ゲームオーバーのBGM(ループしない)
+            audioSource.clip = OverSound;
+            audioSource.loop = false;
+        }
+
         else//ステージ
         {
-            //ステージのBGM
+            //ステージのBGM(ループする)
             audioSource.clip = GameMainBgm;
+            audioSource.loop = true;
         }
-        if(!audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
-        //if(Input.GetKeyDown(KeyCode.Space)&&PlayFlg==false)
-        //{
-        //    Debug.Log("PUSH");
-        //    PlayFlg = true;
-        //    audioSource.Play();
-        //}
-        //else if(Input.GetKeyDown(KeyCode.Space)&&PlayFlg==true)
-        //{
-        //    audioSource.Stop();
-        //    PlayFlg = false;
-        //}
     }
-    void SceneLoaded()
+    void ActiveSceneChanged(Scene thisScene, Scene nextScene)//アクティブシーンが変わったら
     {
-        audioSource.Play();
+        sceneName = nextScene.name;
+        PlayFlg = false;
     }
+
 }
