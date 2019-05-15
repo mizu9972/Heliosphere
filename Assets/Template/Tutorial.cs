21c013bd9146861de8685895c6e7faf3158b21b5
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-16 08:45:48 +0900

    チュートリアル用のPlaneとスクリプトを追加

---
+++ Assets/Template/Tutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //とりあえず画面の半分の割合をTOPに入れる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
