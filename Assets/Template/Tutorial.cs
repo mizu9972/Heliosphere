1a49b5bc7199da8ae081c2ad13e8b8d1ae126b25
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-16 11:11:11 +0900

    移動範囲制限用のPlaneを追加。条件を満たすとフェードアウトで下半分が見えるようになって移動範囲を解放する条件式を実装(現在は仮でSPACEキーが押されたら)

--- Assets/Template/Tutorial.cs
+++ Assets/Template/Tutorial.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 public class Tutorial : MonoBehaviour
 {
+    private RectTransform MyTrans;
     // Start is called before the first frame update
     void Start()
     {
-        //とりあえず画面の半分の割合をTOPに入れる
+        //画面の半分の割合をTOPに入れる
+        MyTrans = GetComponent<RectTransform>();
+        MyTrans.offsetMax = new Vector2(0, -Screen.height / 2.0f);
+        //条件達成でフェードインを実行(現在は仮でSPACEキー)
+        this.UpdateAsObservable().Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ => this.GetComponent<FeedIn>().enabled = true);
+        this.UpdateAsObservable().Where(_ => this.GetComponent<Image>().color.a <= 0)
+            .Subscribe(_ =>Debug.Log("移動範囲を解放します")/*MovePlaneのenabledをfalseにする処理*/);
     }
 
     // Update is called once per frame
