cc17fecbe7407a1069c39feb70cf8c8259fd601a
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-16 12:04:32 +0900

    MovePlane用のスクリプトとインターフェースのスクリプトを追加。Planeのα値が0になったら移動範囲を開放する処理を実装。

---
+++ Assets/Template/Script/ITutorial.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITutorial
+{
+    //移動範囲制限を解除
+    void OpenMoveSpace();
+}
--- Assets/Template/Script/Tutorial.cs
+++ Assets/Template/Script/Tutorial.cs
@@ -7,17 +7,20 @@ using UniRx.Triggers;
 public class Tutorial : MonoBehaviour
 {
     private RectTransform MyTrans;
+    public GameObject MovePlane;//移動範囲制限オブジェクト
     // Start is called before the first frame update
     void Start()
     {
         //画面の半分の割合をTOPに入れる
         MyTrans = GetComponent<RectTransform>();
         MyTrans.offsetMax = new Vector2(0, -Screen.height / 2.0f);
-        //条件達成でフェードインを実行(現在は仮でSPACEキー)
+        //条件達成でフェードアウトを実行(現在は仮でSPACEキー)
         this.UpdateAsObservable().Where(_ => Input.GetKeyDown(KeyCode.Space))
             .Subscribe(_ => this.GetComponent<FeedIn>().enabled = true);
-        this.UpdateAsObservable().Where(_ => this.GetComponent<Image>().color.a <= 0)
-            .Subscribe(_ =>Debug.Log("移動範囲を解放します")/*MovePlaneのenabledをfalseにする処理*/);
+        //フェードアウトが終了したら移動範囲制限解除
+        this.UpdateAsObservable().
+            Where(_ => this.GetComponent<Image>().color.a <= 0&&MovePlane!=null)
+            .Subscribe(_ => MovePlane.GetComponent<ITutorial>().OpenMoveSpace());
     }
 
     // Update is called once per frame
