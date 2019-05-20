bd3b324ea596858c1b3c10da0818a8bf026aeb1c
mizu9972 <oisiimizu9972@icloud.com>
2019-05-20 16:00:23 +0900

    PlanetariumTitle画面でのみ生成し、Destroyされないようにした。

---
+++ Assets/Scenes/Title/DontDestroySet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroySet : MonoBehaviour
+{
+    [SerializeField,Header("破壊されないようにするオブジェクト")]
+    GameObject DontDestroy1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(DontDestroy1);
+    }
+}
