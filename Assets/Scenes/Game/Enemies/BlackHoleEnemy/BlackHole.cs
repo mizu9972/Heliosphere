d2a0a01870bfe9e53d46604c02fc8a6bed67cdd0
mizu9972 <oisiimizu9972@icloud.com>
2019-05-20 13:36:22 +0900

    プレイヤーがブラックホールに引っぱられる動きをするように。 IAffectbyBHインターフェース作成しブラックホール側のCollderにプレイヤーが侵入したら作動。 位置差から重力の与える方向に移動させていく。

---
+++ Assets/Scenes/Game/Enemies/BlackHoleEnemy/BlackHole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //重力の影響を与える
+        //IAffectbyBHインターフェースを継承しているか
+        var isAffectable = other.GetComponent<IAffectbyBH>();
+        
+        if (isAffectable != null)
+        {//与えられるなら
+            other.GetComponent<IAffectbyBH>().GravityEffect(this.GetComponent<Transform>());
+        }
+    }
+}
---
+++ Assets/Scenes/Game/Player/scripts/MoveByGravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByGravity : MonoBehaviour, IAffectbyBH
+{
+    [SerializeField, Header("影響の強さの倍率")]
+    float GravityForceRatio;
+
+    private Transform MyTrans;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void GravityEffect(Transform BHTrans)
+    {
+        MyTrans = this.GetComponent<Transform>();
+
+        //ブラックホールとの位置差
+        Vector3 subVector = new Vector3(
+            BHTrans.position.x - MyTrans.position.x, 
+            BHTrans.position.y - MyTrans.position.y,
+            BHTrans.position.z - MyTrans.position.z
+            );
+
+        subVector = transform.TransformDirection(subVector);
+        subVector *= GravityForceRatio;
+        controller.Move(subVector * Time.deltaTime);
+    }
+}
