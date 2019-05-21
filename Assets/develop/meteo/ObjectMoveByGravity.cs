f059f54d99ec8648ac4c5ac1ccd0ed4e8ec779d1
mizu9972 <oisiimizu9972@icloud.com>
2019-05-21 16:30:11 +0900

    隕石作成。彗星の尾にあたると吹き飛ばされる。吹き飛ぶ強さはインスペクターから操作可。 ブラックホールに吸われる

---
+++ Assets/develop/meteo/ObjectMoveByGravity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMoveByGravity : MonoBehaviour, IAffectbyBH
+{
+    [SerializeField, Header("影響の強さの倍率")]
+    float GravityForceRatio;
+
+
+    private Transform MyTrans;
+    private Rigidbody MyRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRigid = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GravityEffect(Transform BHTrans)
+    {
+        MyTrans = this.GetComponent<Transform>();
+
+        //ブラックホールとの位置差
+        Vector3 subVector = new Vector3(
+            BHTrans.position.x - MyTrans.position.x,
+            BHTrans.position.y - MyTrans.position.y,
+            BHTrans.position.z - MyTrans.position.z
+            );
+
+        subVector = transform.TransformDirection(subVector);
+        subVector *= GravityForceRatio;
+
+        MyRigid.AddForceAtPosition(subVector, MyTrans.position);
+    }
+}
