ed4f093f62c37215f27d777d3f645c6ff1731947
mizu9972 <oisiimizu9972@icloud.com>
2019-05-23 00:22:29 +0900

    フィーバーモードのフロー実装。現在はenemy１体倒したらFeverします。見た目演出はなにもないです

---
+++ Assets/FeverManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeverManager : MonoBehaviour
+{
+    [SerializeField, Header("先頭のWAVE")]
+    GameObject StartFeverWAVE;
+
+    [SerializeField, Header("ゲームマスター")]
+    GameObject GameMaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FeverStart()
+    {
+        StartFeverWAVE.gameObject.SetActive(true);
+        StartFeverWAVE.GetComponent<FeverGameManager>().ApproachStart();
+    }
+
+    public void FeverFinish()
+    {
+        GameMaster.GetComponent<GameMaster>().FeverFinish();
+    }
+}
---
+++ Assets/Template/Script/GameMaster.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class GameMaster : MonoBehaviour
+{
+    [SerializeField,Header("最初のWAVE設定")]
+    GameObject NowWAVEGameManager;
+
+    [SerializeField, Header("フィーバーWAVEプリセット")]
+    GameObject FeverPrehab;
+
+
+    private int enemycount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.UpdateAsObservable().Where(_ => enemycount >= 1).Subscribe(_ => FeverStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void WAVEset(GameObject SetManager)
+    {
+        //進行中のWAVE保存
+        NowWAVEGameManager = SetManager;
+    }
+
+    public void FeverStart()
+    {
+        NowWAVEGameManager.SetActive(false);
+        FeverPrehab.gameObject.SetActive(true);
+        FeverPrehab.GetComponent<FeverManager>().FeverStart();
+        enemycount = 0;
+    }
+
+    public void FeverFinish()
+    {
+        FeverPrehab.gameObject.SetActive(false);
+        NowWAVEGameManager.SetActive(true);
+    }
+
+    public void AddEnemycount()
+    {
+        enemycount += 1;
+    }
+}
