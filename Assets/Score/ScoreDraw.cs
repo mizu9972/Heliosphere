1c1c7f12e4f40b13cbdedf09ef831e7644833453
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-22 20:24:08 +0900

    スコア用の表示処理を実装。常に画面左上に表示するSetScorePosは実装中。WAVEGameManagerからオブジェクトにスコアを渡すように書けば使えます。

---
+++ Assets/Score/ScoreDraw.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreDraw : MonoBehaviour
+{
+    [Header("何桁目か1～5")]
+    public int Dig;//桁数
+    public Canvas Parent;
+    private int Num;//表示する数字
+    private RawImage rawImage;
+    private float U, V;
+    //テクスチャを割り当てるサイズ
+    private float TexWidth = 0.2f;
+    private float TexHeight = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rawImage = this.GetComponent<RawImage>();
+        if(Dig<1||Dig>5)//Digの値が1以上5未満でなければ警告の表示
+        {
+            Debug.Log("Digの値を確認してください");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTexturePosition();//テクスチャに座標をセット
+    }
+    void SetTexturePosition()
+    {
+        if (Parent != null)
+        {
+            //Num = Parent.GetComponent<Score>().GetScoreDigit(Dig);//自分の桁の数字を取得
+            if(Num<5)
+            {
+                V = Num / 5 * 0.5f + 0.5f;
+            }else
+            {
+                V = Num / 5 * 0.5f - 0.5f;
+            }
+            U = Num % 5 * 0.2f;
+            
+            rawImage.uvRect = new Rect(U, V, TexWidth, TexHeight);
+        }
+    }
+}
---
+++ Assets/Score/SetScorePos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetScorePos : MonoBehaviour
+{
+    private Vector3 Pos;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        Pos = this.GetComponent<RectTransform>().position;
+        Pos = GetScreenTopLeft();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Vector3 GetScreenTopLeft()
+    {
+        // 画面の左上を取得
+        Vector3 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+        // 上下反転させる
+        topLeft.Scale(new Vector3(1f, -1f, 1f));
+        return topLeft;
+    }
+}
