64383f00097e59abde56bf88acc3defef2949116
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-24 17:48:44 +0900

    フィーバーの位置セット

---
+++ Assets/Fever/SetFeverPos.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFeverPos : MonoBehaviour
+{
+    private RectTransform MyTrans;
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        MyTrans = this.GetComponent<RectTransform>();
+        MyTrans.anchoredPosition = GetScreenTopLeft();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    Vector3 GetScreenTopLeft()
+    {
+        // 画面の左上を取得
+        Vector3 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+        topLeft.x += 150;
+        topLeft.y += 50;
+        // 上下反転させる
+        topLeft.Scale(new Vector3(1f, -1f, 1f));
+        return topLeft;
+    }
+}
