92fa59f1b8f4b3203d24a75c6bbb274b676e28f4
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-24 15:41:30 +0900

    符号の切り替え実装

---
+++ Assets/Score/DrawSign.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DrawSign : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Parent;
+    private double Score;
+    private RawImage rawImage;
+    private float U, V;
+    //テクスチャを割り当てるサイズ
+    private float TexWidth = 1.0f;
+    private float TexHeight = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rawImage = this.GetComponent<RawImage>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score = Parent.GetComponent<AddScore>().GetScore();
+        if(Score>0)
+        {
+            //＋
+            U = 0;
+            V = 0.5f;
+        }
+        else if(Score<0)
+        {
+            //マイナス
+            U = 0;
+            V = 1.0f;
+        }
+        rawImage.uvRect = new Rect(U, V, TexWidth, TexHeight);
+    }
+}
