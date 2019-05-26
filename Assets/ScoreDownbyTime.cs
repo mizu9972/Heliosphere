ac1f186b2578f965a1de5cc5f8bfaadb4686746e
mizu9972 <oisiimizu9972@icloud.com>
2019-05-27 00:25:07 +0900

    毎秒スコアダウンさせる処理のスクリプト追加

---
+++ Assets/ScoreDownbyTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class ScoreDownbyTime : MonoBehaviour
+{
+
+    [SerializeField, Header("毎秒減少させるスコア")]
+    double DownScore = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var GetScore = this.GetComponent<Score>();
+        Observable.Interval(System.TimeSpan.FromSeconds(0.5f))
+            .Select(_ => GetScore.ScoreGetter())
+            .Where(x => x > 1)
+            .Subscribe(_ => GetScore.ScoreCount(DownScore / 2.0f));
+    }
+
+}
