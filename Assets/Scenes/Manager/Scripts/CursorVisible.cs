1f2da889388906cd4cd35bd1d91cb3022a654b66
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-26 13:24:05 +0900

    フィーバーゲージのマックス状態がどれくらいか見えるようにした。ゲーム画面にマウスカーソルが表示されなくなるスクリプトを作りManagerSceneのManagerにアタッチ。マウスカーソルのせいでボタンが変な挙動したり録画にマウス表示されたりすることがこれでなくなると思います。

---
+++ Assets/Scenes/Manager/Scripts/CursorVisible.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CursorVisible : MonoBehaviour
+{
+    void Start()
+    {
+        // マウスカーソルを削除する
+        UnityEngine.Cursor.visible = false;
+    }
+}
