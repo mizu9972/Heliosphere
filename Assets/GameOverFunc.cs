9f92134bef9becb45e39839526814930f6889741
mizu9972 <oisiimizu9972@icloud.com>
2019-05-27 17:03:30 +0900

    ゲームーオーバー時の彗星の消滅時にだんだん小さくなっていく処理追加

--- Assets/GameOverFunc.cs
+++ Assets/GameOverFunc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
 
 public class GameOverFunc : MonoBehaviour
 {
@@ -17,8 +18,27 @@ public class GameOverFunc : MonoBehaviour
     [SerializeField, Header("コライダー")]
     GameObject ColliderParent;
 
+    [SerializeField, Header("彗星コア")]
+    GameObject CometCore;
+
+    Vector3 CometSize;
+    [SerializeField, Header("CometTrail")]
+    ParticleSystem CometParticle;
+
+    Vector3 CometParticleSize;
+
+
+    void Update()
+    {
+
+    }
+
     public void GameOver()
     {
+        CometSize = CometCore.transform.localScale;
+        CometParticleSize = CometParticle.transform.localScale;
+        CometDeathTime *= 1000;
+
         ColliderParent.SetActive(false);
         DeathParticle.gameObject.SetActive(true);
 
@@ -27,11 +47,24 @@ public class GameOverFunc : MonoBehaviour
         ParticleMain.duration = ParticleTime;
 
         DeathParticle.Play();
+        
+        this.UpdateAsObservable().Subscribe(_ => CometFunc());
 
         Observable.Timer(System.TimeSpan.FromSeconds(ParticleTime)).Subscribe(_ => DeathFunc());
-        Observable.Timer(System.TimeSpan.FromSeconds(CometDeathTime)).Subscribe(_ => this.gameObject.SetActive(false));
+        Observable.Timer(System.TimeSpan.FromMilliseconds(CometDeathTime)).Subscribe(_ => this.gameObject.SetActive(false));
     }
 
+    void CometFunc()
+    {
+        if(CometCore == null || CometParticle == null)
+        {
+            return;
+        }
+
+        CometCore.transform.localScale -= new Vector3(CometSize.x / (CometDeathTime / 16), CometSize.y / (CometDeathTime / 16), CometSize.z / (CometDeathTime / 16));
+        CometParticle.transform.localScale -= new Vector3(CometParticleSize.x / (CometDeathTime / 16), CometParticleSize.y / (CometDeathTime / 16), CometParticleSize.z / (CometDeathTime / 16));
+
+    }
     void DeathFunc()
     {
 
