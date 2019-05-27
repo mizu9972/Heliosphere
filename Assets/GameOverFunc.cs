7b9efad92e2031327ed0e2b6666e68e514273efb
mizu9972 <oisiimizu9972@icloud.com>
2019-05-27 16:14:34 +0900

    ゲームオーバーの時のパーティクル発生するように、CometVer100のインスペクターから設定

--- Assets/GameOverFunc.cs
+++ Assets/GameOverFunc.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class GameOverFunc : MonoBehaviour
 {
+    [SerializeField, Header("死亡演出で生成するパーティクル")]
+    ParticleSystem DeathParticle;
+
+    [SerializeField, Header("パーティクルの時間")]
+    float ParticleTime;
+
+    [SerializeField, Header("彗星が消えるまでの時間")]
+    float CometDeathTime;
+
+    [SerializeField, Header("コライダー")]
+    GameObject ColliderParent;
 
     public void GameOver()
     {
-        this.gameObject.SetActive(false);
+        ColliderParent.SetActive(false);
+        DeathParticle.gameObject.SetActive(true);
+
+        //パーティクルのDurationを設定
+        var ParticleMain = DeathParticle.main;
+        ParticleMain.duration = ParticleTime;
+
+        DeathParticle.Play();
+
+        Observable.Timer(System.TimeSpan.FromSeconds(ParticleTime)).Subscribe(_ => DeathFunc());
+        Observable.Timer(System.TimeSpan.FromSeconds(CometDeathTime)).Subscribe(_ => this.gameObject.SetActive(false));
+    }
+
+    void DeathFunc()
+    {
+
+        DeathParticle.Stop();
     }
 }
