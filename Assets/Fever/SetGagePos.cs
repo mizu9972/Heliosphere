68325db08ee6e754faa580d068504233a31ec1fa
syu_0417 <s_kiba84@yahoo.co.jp>
2019-05-29 12:18:56 +0900

    フィーバーゲージの大きさと位置の調整。フィーバーの文字の大きさと位置の調整

--- Assets/Fever/SetGagePos.cs
+++ Assets/Fever/SetGagePos.cs
@@ -23,7 +23,7 @@ public class SetGagePos : MonoBehaviour
     {
         // 画面の左上を取得
         Vector3 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
-        topLeft.y += 400;
+        topLeft.y += 500;
         topLeft.x += 25;
         // 上下反転させる
         topLeft.Scale(new Vector3(1f, -1f, 1f));
